6f07689c06862bea34ba7db6825d2d6ec3b66641
nhuthuy7122003 <nhuthuy7122003@gmail.com>
2024-12-25 11:38:53 +0700

    last-change

--- lib/screen/login/LoginPage.dart
+++ lib/screen/login/LoginPage.dart
@@ -4,197 +4,240 @@ class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _MyWidgetState createState() => _MyWidgetState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _MyWidgetState extends State<LoginPage> {
+  bool isRegistering = false;
   final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _firstNameController = TextEditingController();
+  final TextEditingController _lastNameController = TextEditingController();
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _confirmPasswordController =
       TextEditingController();
-  final TextEditingController _firstNameController = TextEditingController();
-  final TextEditingController _lastNameController = TextEditingController();
 
-  bool isRegistering = false; // Toggle between Login and Register
+  void _handleGoogleSignIn() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Google sign-in not implemented')),
+    );
+  }
+
+  void _handleFacebookSignIn() {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text('Facebook sign-in not implemented')),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    // Get the height of the screen
     double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      resizeToAvoidBottomInset:
-          true, // Adjust the layout when the keyboard appears
+      resizeToAvoidBottomInset: true,
       body: SingleChildScrollView(
-        // Wrap content in SingleChildScrollView
         child: Container(
           height: screenHeight,
-          width: screenWidth,
           decoration: BoxDecoration(
             image: DecorationImage(
-              image: AssetImage(
-                  'assets/images/kinh-nghiem-ban-gao-le.jpg'), // Ensure the image exists
+              image: AssetImage('assets/images/kinh-nghiem-ban-gao-le.jpg'),
               fit: BoxFit.cover,
               colorFilter: ColorFilter.mode(Colors.black45, BlendMode.darken),
             ),
           ),
           child: Center(
             child: Padding(
-              padding: EdgeInsets.symmetric(
-                  horizontal: 8.0,
-                  vertical: screenHeight *
-                      0.1), // Adjust padding for mobile responsiveness
-              child: Card(
-                color: Colors.white.withOpacity(0.8),
-                elevation: 1.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8.0),
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxWidth: 400, // Giới hạn chiều ngang tối đa
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        isRegistering ? 'Đăng ký' : 'Đăng nhập',
-                        style: TextStyle(
-                            fontSize: 24, fontWeight: FontWeight.bold),
-                      ),
-                      SizedBox(height: 20),
-                      Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            // First Name Field (only for Registering)
-                            if (isRegistering) ...[
+                child: Card(
+                  color: Colors.white.withOpacity(0.8),
+                  elevation: 1.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          isRegistering ? 'Đăng ký' : 'Đăng nhập',
+                          style: TextStyle(
+                              fontSize: 24, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(height: 20),
+                        Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              if (isRegistering) ...[
+                                TextFormField(
+                                  controller: _firstNameController,
+                                  decoration: InputDecoration(
+                                    labelText: 'Họ',
+                                    prefixIcon: Icon(Icons.person),
+                                  ),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Vui lòng nhập họ';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 16),
+                                TextFormField(
+                                  controller: _lastNameController,
+                                  decoration: const InputDecoration(
+                                    labelText: 'Tên',
+                                    prefixIcon: Icon(Icons.person),
+                                  ),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Vui lòng nhập tên';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 16),
+                              ],
                               TextFormField(
-                                controller: _firstNameController,
+                                controller: _emailController,
                                 decoration: InputDecoration(
-                                  labelText: 'Họ',
-                                  prefixIcon: Icon(Icons.person),
+                                  labelText: 'Email',
+                                  prefixIcon: Icon(Icons.email),
                                 ),
                                 validator: (value) {
                                   if (value == null || value.isEmpty) {
-                                    return 'Vui lòng nhập họ';
+                                    return 'Vui lòng nhập Email';
                                   }
                                   return null;
                                 },
                               ),
                               SizedBox(height: 16),
                               TextFormField(
-                                controller: _lastNameController,
-                                decoration: const InputDecoration(
-                                  labelText: 'Tên',
-                                  prefixIcon: Icon(Icons.person),
-                                ),
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Vui lòng nhập tên';
-                                  }
-                                  return null;
-                                },
-                              ),
-                              SizedBox(height: 16),
-                            ],
-                            // Email Field
-                            TextFormField(
-                              controller: _emailController,
-                              decoration: InputDecoration(
-                                labelText: 'Email',
-                                prefixIcon: Icon(Icons.email),
-                              ),
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Vui lòng nhập Email';
-                                }
-                                return null;
-                              },
-                            ),
-                            SizedBox(height: 16),
-                            // Password Field
-                            TextFormField(
-                              controller: _passwordController,
-                              obscureText: true,
-                              decoration: InputDecoration(
-                                labelText: 'Mật khẩu',
-                                prefixIcon: Icon(Icons.lock),
-                              ),
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Vui lòng nhập mật khẩu';
-                                }
-                                return null;
-                              },
-                            ),
-                            SizedBox(height: 16),
-                            // Confirm Password Field (only for Registering)
-                            if (isRegistering) ...[
-                              TextFormField(
-                                controller: _confirmPasswordController,
+                                controller: _passwordController,
                                 obscureText: true,
                                 decoration: InputDecoration(
-                                  labelText: 'Xác nhận mật khẩu',
+                                  labelText: 'Mật khẩu',
                                   prefixIcon: Icon(Icons.lock),
                                 ),
                                 validator: (value) {
                                   if (value == null || value.isEmpty) {
-                                    return 'Vui lòng xác nhận mật khẩu';
-                                  }
-                                  if (value != _passwordController.text) {
-                                    return 'Mật khẩu không khớp';
+                                    return 'Vui lòng nhập mật khẩu';
                                   }
                                   return null;
                                 },
                               ),
-                              SizedBox(height: 20),
-                            ],
-                            ElevatedButton(
-                              onPressed: () {
-                                if (_formKey.currentState?.validate() ??
-                                    false) {
-                                  String email = _emailController.text;
-                                  String password = _passwordController.text;
-                                  // Perform login/register action
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                    SnackBar(
-                                        content: Text('Đang tải dữ liệu...')),
-                                  );
-                                }
-                              },
-                              child:
-                                  Text(isRegistering ? 'Đăng ký' : 'Đăng nhập'),
-                            ),
-                            SizedBox(height: 20),
-                            // Toggle between Login and Register
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text(
-                                  isRegistering
-                                      ? 'Bạn đã có tài khoản'
-                                      : 'Bạn chưa có tài khoản',
-                                  style: TextStyle(color: Colors.black),
-                                ),
-                                SizedBox(width: 20),
-                                TextButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      isRegistering = !isRegistering;
-                                    });
-                                  },
-                                  child: Text(
-                                    isRegistering ? 'Đăng nhập' : 'Đăng ký',
-                                    style: TextStyle(color: Colors.blue),
+                              SizedBox(height: 16),
+                              if (isRegistering) ...[
+                                TextFormField(
+                                  controller: _confirmPasswordController,
+                                  obscureText: true,
+                                  decoration: InputDecoration(
+                                    labelText: 'Xác nhận mật khẩu',
+                                    prefixIcon: Icon(Icons.lock),
                                   ),
+                                  validator: (value) {
+                                    if (value == null || value.isEmpty) {
+                                      return 'Vui lòng xác nhận mật khẩu';
+                                    }
+                                    if (value != _passwordController.text) {
+                                      return 'Mật khẩu không khớp';
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                SizedBox(height: 20),
+                              ],
+                              ElevatedButton(
+                                onPressed: () {
+                                  if (_formKey.currentState?.validate() ??
+                                      false) {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(
+                                          content: Text('Đang tải dữ liệu...')),
+                                    );
+                                  }
+                                },
+                                child: Text(
+                                    isRegistering ? 'Đăng ký' : 'Đăng nhập'),
+                              ),
+                              SizedBox(height: 20),
+                              if (!isRegistering) ...[
+                                SizedBox(height: 20),
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    Flexible(
+                                      child: ElevatedButton.icon(
+                                        onPressed: _handleGoogleSignIn,
+                                        icon: Image.asset(
+                                          'assets/images/google-icon.jpg',
+                                          width: 24,
+                                          height: 24,
+                                          errorBuilder:
+                                              (context, error, stackTrace) {
+                                            return const Icon(Icons.error,
+                                                color: Colors
+                                                    .red); // Xử lý lỗi hiển thị
+                                          },
+                                        ),
+                                        label:
+                                            const Text("Đăng nhập với Google"),
+                                        style: ElevatedButton.styleFrom(
+                                          minimumSize: const Size(0, 50),
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 8),
+                                        ),
+                                      ),
+                                    ),
+                                    const SizedBox(width: 8),
+                                    Flexible(
+                                      child: ElevatedButton.icon(
+                                        onPressed: _handleFacebookSignIn,
+                                        icon: const Icon(Icons.facebook),
+                                        label: const Text(
+                                            "Đăng nhập với Facebook"),
+                                        style: ElevatedButton.styleFrom(
+                                          minimumSize: Size(0, 50),
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 8),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ],
-                            ),
-                          ],
+                              SizedBox(height: 20),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    isRegistering
+                                        ? 'Bạn đã có tài khoản'
+                                        : 'Bạn chưa có tài khoản',
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        isRegistering = !isRegistering;
+                                      });
+                                    },
+                                    child: Text(
+                                      isRegistering ? 'Đăng nhập' : 'Đăng ký',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
@@ -203,5 +246,15 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _firstNameController.dispose();
+    _lastNameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _confirmPasswordController.dispose();
+    super.dispose();
   }
 }
